228e8713735205075db8f18f6557131028684a01
Obeyed46 <37306477+Obeyed46@users.noreply.github.com>
2018-10-14 18:53:59 +0200

    Commit20
    Commit20

--- 2dHackAndSlashGame/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ 2dHackAndSlashGame/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -29,6 +29,9 @@ public class PlayerController : MonoBehaviour {
     public float weaponRange, weaponDamage;
     public LayerMask enemiesLayer;
 
+    //Dash variables
+    bool CanDash, IsDashing;
+
     // Use this for initialization
     void Start () {
 
@@ -38,6 +41,8 @@ public class PlayerController : MonoBehaviour {
         FacingRight = true;
         CanMove = true;
         CanJump = true;
+        CanDash = true;
+        IsDashing = false;
         Attack2 = false;
         Attack3 = false;
 
@@ -58,9 +63,11 @@ public class PlayerController : MonoBehaviour {
             CanMove = true;
             CanJump = true;
             CanAttack = true;
+            CanDash = true;
             Attack2 = false;
             Attack3 = false;
         }
+
 
         //Jump
         if (Grounded && CanJump && Input.GetKeyDown(KeyCode.Joystick1Button0))
@@ -94,14 +101,19 @@ public class PlayerController : MonoBehaviour {
 
         }
 
-        //Do not move if Attacking
-        if(MyAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack3") || MyAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack2") || MyAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack1") || MyAnim.GetCurrentAnimatorStateInfo(0).IsName("HeavyAttack"))
+        //Do not move if Attacking or 
+        if(MyAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack3") || MyAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack2") || MyAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack1") || MyAnim.GetCurrentAnimatorStateInfo(0).IsName("HeavyAttack") || MyAnim.GetCurrentAnimatorStateInfo(0).IsName("Dash"))
         {
             CanMove = false;
             CanJump = false;
+            CanDash = false;
         }
 
-
+        //Dashing
+        if(Input.GetKeyDown(KeyCode.Joystick1Button1) && CanDash)
+        {
+            MyAnim.SetTrigger("Dashing");
+        }
 
     }
 
@@ -133,8 +145,20 @@ public class PlayerController : MonoBehaviour {
                 }
             }
 
-        }
+            //DashSprint
+            if (IsDashing)
+            {
+                if (FacingRight)
+                {
+                    MyRb.velocity = new Vector2(4f, MyRb.velocity.y);
+                }
+                else if (!FacingRight)
+                {
+                    MyRb.velocity = new Vector2(-4f, MyRb.velocity.y);
+                }
+            }
 
+        }
 
     }
 
@@ -189,11 +213,21 @@ public class PlayerController : MonoBehaviour {
             MyRb.velocity = new Vector2(-2f, MyRb.velocity.y);
         }
 
-    }
+    }  
 
     public void EndAttackSprint()
     {
         MyRb.velocity = new Vector2(0, MyRb.velocity.y);
+    }
+
+    public void DashSprint()
+    {
+        IsDashing = true;
+    }
+
+    public void EndDashSprint()
+    {
+        IsDashing = false;
     }
 
     public void CheckEnemies()
